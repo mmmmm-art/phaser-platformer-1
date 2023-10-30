d653f82d746fc73881e8699de9bd0818189ef97c
Robert Hardy <rhardy1@dcskk12.org>
2023-10-30 16:23:26 -0600

    Add image

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 import "./style.css";
 import Phaser from "phaser";
 
-/** @type {Phaser.Core.Config} */
+class MainScene extends Phaser.Scene {
+	constructor() {
+		super("main-scene");
+	}
+
+	preload() {
+		this.load.atlas("robot", "robot.png", "robot.json");
+	}
+
+	create() {
+		// const height = this.scale.height;
+		// const width = this.scale.width;
+
+		// object destructuring
+		const { height, width } = this.scale;
+
+		let player = this.add.sprite(width / 2, height / 2, "robot");
+	}
+
+	update() {}
+}
+
+/** @type {Phaser.Types.Core.GameConfig} */
 const config = {
 	type: Phaser.WEBGL,
 	width: 400,
 	height: 400,
+	scene: [MainScene],
 };
 
 const game = new Phaser.Game(config);
